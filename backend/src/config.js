b108ca1bce74a35df0d7f47919e90605d1a7a396
Marcos Garzon <marckosgarzon1@hotmail.com>
2025-05-03 11:44:42 -0500

    Se modifica forma de exponer de ECS a miniKube

--- backend/src/config.js
+++ backend/src/config.js
@@ -3,4 +3,4 @@ config();
 
 export const PORT = process.env.PORT || 4000;
 export const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://mongodb.mern-app:27017/merndatabase";
+  process.env.MONGODB_URI || "mongodb://mongo-service:27017/merndatabase";
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: { // Añadir esta sección
+    proxy: {
+      '/api': {
+        // Reemplaza con la URL de tu servicio backend en Minikube
+        target: 'http://backend-service:4000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
